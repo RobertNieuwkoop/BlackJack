f958e3bde5e5351708ea6bf3ebbd2cb7369ccc94
RobertNieuwkoop <95620023+RobertNieuwkoop@users.noreply.github.com>
2025-02-19 08:43:52 +0100

    Added button1 click event

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -18,9 +18,9 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
-
+            Console.WriteLine("Hello World");
         }
     }
 }
